6ecf3196ea0f6285184bd100e205a589434cf9a9
SuhailAhamed2000 <81544828+SuhailAhamed2000@users.noreply.github.com>
2024-03-05 12:32:05 -0800

    Add files via upload

--- Checkpoint1.sv
+++ Checkpoint1.sv
@@ -1,109 +1,89 @@
-module GenericIOM(ALE,Clock,Reset,A,AD,RD,WR,CS);
-	
-	parameter IOM = 0;
-	parameter AddLines = 20;
-	parameter IOLines = 16;
-	input logic ALE,Clock,RD,WR,CS,Reset;
-	inout logic [7:0] AD;
-	input logic [19:8] A;
-	
-	logic [19:0] Add_Reg;
-	logic [7:0] RegArray [2^AddLines-1 : 0];
-	//logic [7:0] IOReg [2^IOLines-1 : 0];
-	logic load,OE,Wrenb;
-	
-	typedef enum logic [4:0] {
-	
-		IDLE = 5'b00001,
-		ADDLOAD =5'b00010,
-		READ = 5'b00100,
-		WRITE = 5'b01000,
-		DELAY = 5'b10000
-	
-	} State;
-	
-	State Cur_State , Nxt_State;
-	
-	always_ff @(posedge Clock)
-	begin
-		if(Reset)
-			Cur_State <= IDLE;
-		else
-			Cur_State <= Nxt_State;
-	end
-	
-	always_comb
-	begin
-		{load,OE,Wrenb} = 3'b000;
-		case(Cur_State)
-			IDLE	:	{load,OE,Wrenb} = 3'b000;
-			ADDLOAD	:	load = 1;
-			READ	:	OE = 1;
-			WRITE	:	Wrenb = 1;
-			DELAY	:	{load,OE,Wrenb} = 3'b000;
-		endcase
-	end
-	
-	always_comb
-	begin
-		Nxt_State = Cur_State;
-		case(Cur_State)
-			IDLE	:	begin
-							if(ALE)
-								Nxt_State = ADDLOAD;
-							else
-								Nxt_State = IDLE;
-						end
-						
-			ADDLOAD	:	begin
-							if(RD)
-								Nxt_State = READ;
-							else if(WR)
-								Nxt_State = WRITE;
-							else
-								Nxt_State = ADDLOAD;
-						end
-			READ	:	Nxt_State = DELAY;
-			WRITE	:	Nxt_State = DELAY;
-			DELAY	:	Nxt_State = IDLE;
-		endcase
-	end
-	
-	always_latch
-	begin
-		if (load)
-			Add_Reg <= {A, AD};	
-	end
-	
-	// always_comb
-	// begin
-		// if(IOM)
-		// begin
-			// AD = OE ? IOReg[Add_Reg] : 'z;
-			// IOReg[Add_Reg] = Wrenb ? AD : 'z;
-		// end
-		// else
-		// begin
-			// AD = OE ? RegArray[Add_Reg] : 'z;
-			// RegArray[Add_Reg] = Wrenb ? AD : 'z;
-		// end
-	// end
-	
-	always_ff @(posedge Clock)
-	begin
-		RegArray[Add_Reg] = Wrenb ? AD : 'z;
-	end
-	
-	assign AD = OE ? RegArray[Add_Reg] : 'z;
+// Code your design here
+module mpFSM(clk,reset,ALE,RD,WR,CS,A,AD);
+ parameter address=20;
+ input logic clk,reset;
+ inout logic [7:0]AD;
+ input logic [19:8]A;
+ input logic RD,WR,CS,ALE;
 
-	
+ logic load,oe,wrenb;
+
+ logic [19:0]addreg;
+ logic [7:0]regarray[2**address-1:0];
+
+typedef enum logic [4:0]{
+	IDLE  =5'b00001,
+	ADDLD =5'b00010,
+	READ  =5'b00100,
+	WRITE =5'b01000,
+	DELAY =5'b10000
+}state;
+
+state present_state,next_state;
+
+always_ff @(posedge clk)
+  begin
+	if(reset)
+	  present_state<=IDLE;
+    else
+	  present_state<=next_state;
+  end
+
+//output logic
+always_comb
+  begin
+   {load,oe,wrenb}=3'b000;
+  	  case(present_state)
+       IDLE:{load,oe,wrenb}=3'b000;
+	   ADDLD:load=1;
+	   READ:oe=1;
+	   WRITE:wrenb=1;
+       DELAY:{load,oe,wrenb}=3'b000;
+	  endcase
+  end
+
+
+//nextstate logic
+always_comb
+  begin
+	next_state = present_state;
+    case(present_state)
+     IDLE:begin
+	      if(ALE)
+	        next_state=ADDLD;
+	      else
+	        next_state=IDLE;
+	      end
+	 ADDLD:begin
+	       if(~RD)
+	         next_state=READ;
+	       else if(~WR)
+	         next_state=WRITE;
+	       end
+	 READ:begin
+	      if(RD)
+	        next_state=DELAY;
+	      end
+     WRITE:begin
+	       if(WR)
+	         next_state=DELAY;
+	       end
+     DELAY:next_state=IDLE;
+   endcase
+end
+
+always_latch
+  begin
+    if(load)
+    addreg<={A,AD};
+  end
+
+always_ff @(posedge clk)
+  begin
+	regarray[addreg]=wrenb?AD:'z;
+  end
+assign AD=oe?regarray[addreg]:'z;
 endmodule
 
-	
-	
-			
-	
-	
-							
-	
-	
+
+
